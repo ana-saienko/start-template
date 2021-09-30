c3660cfd9cb610c7f1f6fbf6b870024a3a4cba46
saienko-ana <saienko_ana@icloud.com>
2021-09-30 12:05:36 +0200

    tried to fix issues with the burger

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 // Меню бургер
 
 const iconMenu = document.querySelector('.icon-menu');
+const menuBody = document.querySelector('.menu__body');
+const menuLink = document.querySelector('.header__menu-item');
 
 if (iconMenu) {
-	const menuBody = document.querySelector('.menu__body');
+	
 	iconMenu.addEventListener("click", function(e){
 		document.body.classList.toggle('lock');
 		iconMenu.classList.toggle('burger-active');
